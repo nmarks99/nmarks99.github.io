e894932055a707a79f9f6544db381e66d87ca424
Nick <nmarks99@gmail.com>
2022-01-01 13:09:46 -0600

    add comments

--- update_resume.py
+++ update_resume.py
@@ -2,12 +2,13 @@ import tkinter as tk
 from tkinter import filedialog
 import os
     
-# Import flight data
+# Select new resume with file explorer dialog 
 root = tk.Tk()
 root.withdraw()
 resume_path = filedialog.askopenfilename(initialdir="~/OneDrive/Documents/Career/Resumes") # Ask user to select data
 print('\n\nPath to resume: %s\n' % resume_path)
 root.destroy()
 
+# Copy the file to the files/ directory, then add, commit, and push
 os.system("cp {} ./files".format(resume_path))
 os.system("git add --all && git commit -a -m'update resume' && git push")
